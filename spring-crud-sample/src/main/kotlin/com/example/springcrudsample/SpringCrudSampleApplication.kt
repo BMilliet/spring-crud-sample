7d254922e33e373a9fb7a95b4f88f31e0598ae7f
BMilliet <brunomilliet89@gmail.com>
2023-02-03 08:10:27 -0300

    init proj

---
+++ spring-crud-sample/src/main/kotlin/com/example/springcrudsample/SpringCrudSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springcrudsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCrudSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringCrudSampleApplication>(*args)
+}
